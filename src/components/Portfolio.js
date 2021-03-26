a6d51dd2b4fb139fc0ae4115da631e3098b175c3
OnniBjorn <onni.bjorn@edu.salpaus.fi>
2021-03-26 08:12:23 +0000

    3262021

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,30 +3,25 @@ import React from 'react';
 
 function Portfolio() {
   return (
-    <div>
-    <h1>Onni Björn - Portfolio</h1>
-        <h2>Tietoa</h2> 
-    <div class ="tieto">
+<div>
+   <h1>Onni Björn - Portfolio</h1>
+    <h2>Tietoa</h2> 
       Olen 19-vuotias rauhallinen opiskelija. Harrastuksiini kuuluu päivittäinen videopelaaminen. 
-    </div>
-        <h2>Koulutus</h2>
-    <div class="koulu">
+      
+    <h2>Koulutus</h2>
       Peruskoulu (Kärpäsen koulu) 2007-2016<br />
       Kymppiluokka (Lahden kansanopisto) 2016-2017 
-    </div>
+ 
+    <h2>Työkokemus</h2>       
+      Peruskoulun Tet-jakso - Siwa 3-vko<br />
+      Kymppiluokan Tet-jakso - Arjen Aarteet kirpputori 1-vko
 
-     <div class="tyokokemus">
-        <h2>Työkokemus</h2>       
-          Peruskoulun Tet-jakso - Siwa 3-vko<br />
-          Kymppiluokan Tet-jakso - Arjen Aarteet kirpputori 1-vko
-      </div>
     <h2>Kielitaito</h2>
-    <div class ="taito"> 
       Suomi - Äidinkieli<br />
       Englanti - Hyvä<br />
       Ruotsi - Perusteet
-    </div>
-    </div>
+
+  </div>
   )
 }
 
